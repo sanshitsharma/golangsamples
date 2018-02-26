80920c3d7999d09d5731d566798fd231a1b4964f
Sanshit Sharma <sansshar@cisco.com>
2018-02-26 00:24:31 -0800

    LinkedList implementation in ds package

---
+++ ds/linked_list/linkedList.go
@@ -0,0 +1,79 @@
+package linked_list
+
+import "fmt"
+
+type Node struct {
+	Data interface{}
+	Next *Node
+}
+
+type List struct {
+	Head *Node
+}
+
+func InitList() *List {
+	return &List{Head: nil}
+}
+
+func (list *List) Insert(data interface{}) {
+	if list.Head == nil {
+		list.Head = &Node{Data: data, Next: nil}
+		return
+	}
+	
+	curr := list.Head
+	for curr.Next != nil {
+		curr = curr.Next
+	}
+
+	// We are at the last element now
+	curr.Next = &Node{Data: data, Next: nil}
+	return
+}
+
+func (list *List) InsertAtHead(data interface{}) {
+	if list.Head == nil {
+		list.Head = &Node{Data: data, Next: nil}
+		return
+	}
+
+	temp := &Node{Data: data, Next: list.Head}
+	list.Head = temp
+}
+
+func (list *List) Reverse() {
+	if list.Head == nil {
+		fmt.Println("List is empty")
+		return
+	}
+
+	curr := list.Head
+	n := curr.Next
+
+	for n != nil {
+		temp := n.Next
+
+		n.Next = curr
+		if curr == list.Head {
+			curr.Next = nil
+		}
+
+		curr = n
+		n = temp
+	}
+
+	list.Head = curr
+}
+
+//func (list *List) ReverseFromIndex()
+
+func (list *List) Print() {
+	curr := list.Head
+
+	for curr.Next != nil {
+		fmt.Printf("%v --> ", curr.Data)
+		curr = curr.Next
+	}
+
+	fmt.Printf("%v\n", curr.Data)
+}
