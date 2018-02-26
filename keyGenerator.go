d62672c6b146f935613a4c148a5b19b84c692f13
Sanshit Sharma <sansshar@cisco.com>
2018-02-26 00:29:42 -0800

    Added generateKey & generateMany functions

--- keyGenerator.go
+++ keyGenerator.go
@@ -7,45 +7,58 @@ import (
 	"encoding/pem"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
-func main() {
+func generateMany() {
 	reader := rand.Reader
-	bitSize := 512
+	bitSize := 2048
 
 	var totalTime time.Duration
 	iter := 100
 
 	for i := 0; i < iter; i++ {
 		start := time.Now()
-		//key, err := rsa.GenerateKey(reader, bitSize)
-		_, err := rsa.GenerateKey(reader, bitSize)
+		key, err := rsa.GenerateKey(reader, bitSize)
 		generateTime := time.Now().Sub(start)
 
 		totalTime += generateTime
-
 		checkError(err)
 
 		/*
-		fmt.Println("Private key primes", key.Primes[0].String(), key.Primes[1].String())
-		fmt.Println("Private key exponent", key.D.String())
+			fmt.Println("Private key primes", key.Primes[0].String(), key.Primes[1].String())
+			fmt.Println("Private key exponent", key.D.String())
 
-		publicKey := key.PublicKey
-		fmt.Println("Public key modulus", publicKey.N.String())
-		fmt.Println("Public key exponent", publicKey.E)
+			publicKey := key.PublicKey
+			fmt.Println("Public key modulus", publicKey.N.String())
+			fmt.Println("Public key exponent", publicKey.E)
 		*/
 
 		//fmt.Println(reflect.TypeOf(key))
 		//fmt.Println("Iter[", i, "] --> Time to generate RSA key =", generateTime)
 
 		// Save File
-		//pemFilePath := "id_rsa_" + strconv.FormatInt(int64(bitSize), 10)
-		//savePEMKey(pemFilePath, key)
+		pemFilePath := "id_rsa_" + strconv.FormatInt(int64(bitSize), 10)
+		savePEMKey(pemFilePath, key)
 	}
 
-	avgTime := totalTime.Nanoseconds()/int64(iter)
+	avgTime := totalTime.Nanoseconds() / int64(iter)
 	fmt.Println("Average time =", time.Duration(avgTime))
+}
+
+func generateKey(bitsize int, outFile string) error {
+	reader := rand.Reader
+
+	// Generate the key
+	key, err := rsa.GenerateKey(reader, bitsize)
+	if err != nil {
+		return err
+	}
+
+	// Save to file
+	savePEMKey(outFile, key)
+	return nil
 }
 
 func savePEMKey(fileName string, key *rsa.PrivateKey) {
@@ -70,4 +83,11 @@ func checkError(err error) {
 		fmt.Println("Fatal error ", err.Error())
 		os.Exit(1)
 	}
+}
+
+func main() {
+	bitsize := 4096
+	outFile := `/Users/sansshar/go/src/github.com/sanshitsharma/abridge/ushort.pem`
+
+	generateKey(bitsize, outFile)
 }
